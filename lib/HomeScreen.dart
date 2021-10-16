dea31116f2e9f2f67f815077c7fd62f7680a58a9
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-10-16 10:55:24 +0530

    Login basics

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,19 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:geo_spatial/main.dart' as main;
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient : LinearGradient(
-            begin: Alignment(-0.2401895672082901,0.7598104476928711),
-            end: Alignment(-0.7598104476928711,-0.7598104476928711),
-            colors: [Color.fromRGBO(255, 0, 229, 0.7599999904632568),Color.fromRGBO(255, 0, 0, 0.6000000238418579)]
-        ),
+    return MaterialApp(
+      theme: main.theme,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Stack(children: [
+          Container(
+            constraints: BoxConstraints.expand(),
+            // decoration: BoxDecoration(
+            //  gradient: LinearGradient(
+            //       begin: Alignment(2.5,0.5),
+            //       end: Alignment(-0.4,-6.818380082762587e-9),
+            //       colors: [Color.fromRGBO(0, 255, 71, 0.6000000238418579),Color.fromRGBO(0, 189, 177, 0.7599999904632568),]
+            //   ),),
+          ),
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                    padding: EdgeInsets.only(left: 30, top: 40),
+                    child: Text(
+                      'Geo Title (temp)',
+                      style: GoogleFonts.montserrat(
+                          fontSize: 40,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold),
+                    )),
+                Padding(
+                  padding: EdgeInsets.only(left: 30, top: 20),
+                  child: Text(
+                    'Login',
+                    style: GoogleFonts.montserrat(
+                        fontSize: 35,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 200,
+                ),
+                // TODO: Add an image for login
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0, top: 20.0),
+                  child: Container(
+                    width: 250,
+                    alignment: Alignment.center,
+                    child: TextField(
+                      style: GoogleFonts.montserrat(fontSize: 15),
+                      decoration: InputDecoration(
+                          labelText: 'Phone no',
+                          labelStyle: GoogleFonts.montserrat(fontSize: 12)),
+                      keyboardType: TextInputType.phone,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0, top: 20.0),
+                  child: Container(
+                    width: 250,
+                    child: TextField(
+                      style: GoogleFonts.montserrat(fontSize: 15),
+                      decoration: InputDecoration(
+                          labelText: 'Password',
+                          labelStyle: GoogleFonts.montserrat(fontSize: 12),
+                          focusColor: Colors.greenAccent,
+                          hoverColor: Colors.greenAccent),
+                      keyboardType: TextInputType.visiblePassword,
+                    ),
+                  ),
+                ),
+                Container(
+                    width: 200,
+                    padding: EdgeInsets.only(left: 30, top: 50),
+                    child: OutlinedButton.icon(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.arrow_forward_ios_rounded,
+                        color: Colors.black,
+                      ),
+                      label: Text(
+                        "LOGIN",
+                        style: GoogleFonts.montserrat(
+                            fontSize: 15, color: Colors.black),
+                      ),
+                    )),
+              ],
+            ),
+          )
+        ]),
       ),
-    child: Text(')',)
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:geo_spatial/HomeScreen.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: MyApp(),
+      ),
+    ),
+  );
+}
+
+ThemeData theme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: Color(0xff5EAAA8),
+  canvasColor: Color(0xffF7F3E9),
+  accentColor: Color(0xffF05945),
+  scaffoldBackgroundColor: Color(0xffF7F3E9),
+  cardColor: Colors.white,
+  focusColor: Color(0xff5EAAA8),
+  hoverColor: Color(0xff5EAAA8),
+  highlightColor: Color(0xff5EAAA8),
+  disabledColor: Colors.grey.shade200,
+  buttonTheme: ButtonThemeData(
+    minWidth: 200,
+    height: 50,
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(20))),
+  ),
+  buttonColor: Color(0xffA3D2CA),
+  backgroundColor: Color(0xffF7F3E9),
+  hintColor: Color(0xffF05945),
+);
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
