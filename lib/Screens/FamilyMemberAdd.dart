e9a493e96ff6c2ab4234fec323386776a3873c03
Nirmal Karthikeyan <nirmalkarthikeyan1503@gmail.com>
2021-10-19 17:59:53 +0530

    Added new dependency - table_calendar

--- lib/Screens/FamilyMemberAdd.dart
+++ lib/Screens/FamilyMemberAdd.dart
@@ -8,8 +8,35 @@ class FamilyMemberAdd extends StatefulWidget {
 }
 
 class _FamilyMemberAddState extends State<FamilyMemberAdd> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Form(
+              key: _formKey,
+                child: Column(children: [
+                  TextFormField(
+                    validator: (value){
+                      if(value==null || value.isEmpty){
+                        return 'Please enter the name';
+                      }
+                      return null;
+                    },
+                  ),
+
+                  OutlinedButton(onPressed: (){
+                    if(_formKey.currentState!.validate()==true){
+                      print('Form submitted successfully');
+                      //TODO: Add logic to store locally and to lazy upload to database
+                    }
+                  }, child: Text('Submit'))
+            ],))
+          ],
+        ),
+      )
+    );
   }
 }
