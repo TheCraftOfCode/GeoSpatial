ba46d41d710681a9c686899b4093477a9fe2a407
Soorya Senthil Rajan <62637965+SooryaSRajan@users.noreply.github.com>
2021-10-23 22:10:46 +0530

    Added code to lock orientation

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:geo_spatial/Screens/Login.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:geo_spatial/Screens/Home.dart';
+import 'package:flutter/services.dart';
 
 final storage = FlutterSecureStorage();
 
 void main() {
+
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
