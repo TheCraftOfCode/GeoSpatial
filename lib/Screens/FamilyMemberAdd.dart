3ea897296132cb082b810085a47dad0e66cb3817
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2021-11-15 13:09:30 +0530

    Added more widgets

--- lib/Screens/FamilyMemberAdd.dart
+++ lib/Screens/FamilyMemberAdd.dart
@@ -34,146 +34,237 @@ class _FamilyMemberAddState extends State<FamilyMemberAdd> {
       resizeToAvoidBottomInset: false,
       backgroundColor: Color(0xffEAE7FA),
       appBar: AppBarBackButton('Add Family Member'),
-      body:SizedBox(
-        height: MediaQuery.of(context).size.height-MediaQuery.of(context).viewInsets.bottom*1.1,
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height -
+            MediaQuery.of(context).viewInsets.bottom * 1.1,
         child: SingleChildScrollView(
           physics: ClampingScrollPhysics(),
           child: FormPageView([
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        TextFormField(
-                          decoration: InputDecoration(
-                            label: Text("Name"),
-                          ),
-                          validator: (value) {
-                            if (value == "") {
-                              return "Please enter a name";
-                            } else
-                              return null;
-                          },
-                          autovalidateMode: AutovalidateMode.always,
-                        ),
-                        DatePicker("Date of Birth"),
-                        GenderPickerWithImage(
-                          verticalAlignedText: false,
-                          selectedGender: Gender.Male,
-                          selectedGenderTextStyle: TextStyle(
-                              color: Color(0xFF8b32a8), fontWeight: FontWeight.bold),
-                          unSelectedGenderTextStyle:
-                              TextStyle(color: Colors.black, fontWeight: FontWeight.normal),
-                          onChanged: (Gender? gender) {
-                            print(gender);
-                          },
-                          equallyAligned: true,
-                          animationDuration: Duration(milliseconds: 300),
-                          isCircular: true,
-                          // default : true,
-                          opacityOfGradient: 0.4,
-                          padding: const EdgeInsets.all(3),
-                          size: 70, //default : 40
-                        ),
-                        DropDownFormField(
-                          list: [
-                            'None',
-                            'Elementary',
-                            'Secondary',
-                            'Higher Secondary',
-                            'Bachelor\'s',
-                            'Master\'s'
-                          ],
-                          hint: "Select the highest",
-                          title: "Educational qualification",
-                          errorField: "Please choose a qualification",
-                        ),
-                        TextFormField(
-                          keyboardType: TextInputType.number,
-                          decoration: InputDecoration(
-                            label: Text("Phone Number: "),
-                          ),
-                          validator: (value) {
-                            if (value == "") {
-                              return "Please enter a value";
-                            } else if (value!.length != 10) {
-                              return "Enter a valid number";
-                            } else
-                              return null;
-                          },
-                          autovalidateMode: AutovalidateMode.always,
-                        ),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        TextFormField(
-                          validator: (value) {
-                            if (value == "") {
-                              return "Enter 12 digit Aadhaar";
-                            } else if (value!.length != 12) {
-                              return "Enter a valid Aadhaar";
-                            } else
-                              return null;
-                          },
-                          autovalidateMode: AutovalidateMode.always,
-                        ),
-                        DropDownFormField(
-                          //TODO: Replace with a CheckBoxListTile
-                          list: ['Widower', 'Divorcee', 'Differently abled'],
-                          hint: "Select applicable",
-                          title: "Vulnerabilities",
-                          errorField: "Please choose a vulnerability",
-                        ),
-                        DropDownFormField(
-                          list: [
-                            'Clerical support worker',
-                            'Services and sales worker',
-                            'Skilled agricultural, forestry and fishery worker',
-                            'Unemployed'
-                          ],
-                          hint: "Select applicable",
-                          title: "Occupation",
-                          errorField: "Please choose an occupation",
-                        ),
-                        Container(
-                          child: Column(
-                            children: [
-                              OptionsWidget(
-                                options: [["Yes",true],["No",false]],
-                                title: "Daily wage worker?",
-                                onSaved: (val) {
-                                  //dailyWageWorker = value.toLowerCase() as bool;
-                                  print(val.toString());
-                                },
-                              ),
-                            ],
-                          ),
-                        ),
-                        //TODO: Add work timings
-                      ],
-                    ),
-                    Column(children: [
-                      Container(
-                        child: Column(
-                          children: [
-                            OptionsWidget(
-                              options: [
-                                ["Eligible","eligible"],
-                                ["Eligible, receiving","eligible_receiving"],
-                                ["Not eligible","not_eligible"]
-                              ],
-                              title: 'Old age pension',
-                            ),
-                          ],
-                        ),
-                      ),
-                    ]),
-                    Text('Haha Hi')
-                  ], _onSubmit),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    label: Text("Name"),
+                  ),
+                  validator: (value) {
+                    if (value == "") {
+                      return "Please enter a name";
+                    } else
+                      return null;
+                  },
+                  autovalidateMode: AutovalidateMode.always,
+                ),
+                DatePicker("Date of Birth"),
+                GenderPickerWithImage(
+                  verticalAlignedText: false,
+                  selectedGender: Gender.Male,
+                  selectedGenderTextStyle: TextStyle(
+                      color: Color(0xFF8b32a8), fontWeight: FontWeight.bold),
+                  unSelectedGenderTextStyle: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.normal),
+                  onChanged: (Gender? gender) {
+                    print(gender);
+                  },
+                  equallyAligned: true,
+                  animationDuration: Duration(milliseconds: 300),
+                  isCircular: true,
+                  // default : true,
+                  opacityOfGradient: 0.4,
+                  padding: const EdgeInsets.all(3),
+                  size: 70, //default : 40
+                ),
+                DropDownFormField(
+                  list: [
+                    'None',
+                    'Elementary',
+                    'Secondary',
+                    'Higher Secondary',
+                    'Bachelor\'s',
+                    'Master\'s'
+                  ],
+                  hint: "Select the highest",
+                  title: "Educational qualification",
+                  errorField: "Please choose a qualification",
+                ),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    label: Text("Phone Number: "),
+                  ),
+                  validator: (value) {
+                    if (value == "") {
+                      return "Please enter a value";
+                    } else if (value!.length != 10) {
+                      return "Enter a valid number";
+                    } else
+                      return null;
+                  },
+                  autovalidateMode: AutovalidateMode.always,
+                ),
+              ],
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    label: Text('Aadhaar Number'),
+                  ),
+                  validator: (value) {
+                    if (value == "") {
+                      return "Enter 12 digit Aadhaar";
+                    } else if (value!.length != 12) {
+                      return "Enter a valid Aadhaar";
+                    } else
+                      return null;
+                  },
+                  autovalidateMode: AutovalidateMode.always,
+                ),
+                DropDownFormField(
+                  //TODO: Replace with a CheckBoxListTile
+                  list: [
+                    'Widower',
+                    'Divorcee',
+                    'Differently abled',
+                    'Pregnant Woman',
+                    'Lactating Mother',
+                    'Elderly (>60 years)',
+                    'Widower',
+                    'Differently Abled',
+                    'Children below 2 years',
+                    'Others',
+                    'None'
+                  ],
+                  hint: "Select applicable",
+                  title: "Vulnerabilities",
+                  errorField: "Please choose a vulnerability",
+                ),
+                DropDownFormField(
+                  //TODO: Replace with a CheckBoxListTile
+                  list: [
+                    'Clerical support worker',
+                    'Services and sales worker',
+                    'Agricultural, forestry, fishery worker',
+                    'Unemployed',
+                    'Professional',
+                    'Technician and associate professionals',
+                    'Craft and related trades workers',
+                    'Plant/Machine Operators and Assemblers',
+                    'Elementary occupations',
+                    'Armed Forces occupations'
+                  ],
+                  hint: "Select applicable",
+                  title: "Occupation",
+                  errorField: "Please choose an occupation",
+                ),
+                OptionsWidget(
+                  options: [
+                    ["Yes", true],
+                    ["No", false]
+                  ],
+                  title: "Daily wage worker?",
+                  onSaved: (val) {
+                    //dailyWageWorker = value.toLowerCase() as bool;
+                    print(val.toString());
+                  },
+                ),
+                TextFormField(
+                  decoration: InputDecoration(label: Text('Income/day')),
+                  validator: (value) {
+                    if (value == "") {
+                      return "Enter Income/Day";
+                    }
+                  },
+                )
+              ],
+            ),
+            Column(children: [
+              //TODO: Add work timings
+              TextFormField(
+                decoration: InputDecoration(label: Text('Income/month')),
+                validator: (value) {
+                  if (value == "") {
+                    return "Enter Income/Day";
+                  }
+                },
+              ),
+              Column(
+                children: [
+                  OptionsWidget(
+                    options: [
+                      ["Eligible", "eligible"],
+                      ["Eligible, receiving", "eligible_receiving"],
+                      ["Not eligible", "not_eligible"]
+                    ],
+                    title: 'Old age pension',
+                  ),
+                ],
+              ),
+              OptionsWidget(
+                options: [
+                  ["Yes", "yes"],
+                  ["No", "no"],
+                ],
+                title: 'Doing/Did business',
+              ),
+              OptionsWidget(
+                options: [
+                  ["Married", "married"],
+                  ["Unmarried", "unmarried"],
+                ],
+                title: 'Marital Status',
+              ),
+              TextFormField(
+                //TODO: Replace with a custom widget
+                decoration: InputDecoration(label: Text('Special Skills')),
+              ),
+            ]),
+            Column(
+              children: [
+                TextFormField(
+                  //TODO: Replace with a custom widget
+                  decoration: InputDecoration(label: Text('Frequent ailments')),
+                ),
+                TextFormField(
+                  //TODO: Replace with a custom widget
+                  decoration: InputDecoration(label: Text('Communicable Diseases')),
+                ),
+                TextFormField(
+                  //TODO? Replace with a custom widget
+                  decoration: InputDecoration(label: Text('Non Communicable Diseases')),
+                ),
+                OptionsWidget(
+                  options: [
+                    ["Yes", "yes"],
+                    ["No", "no"],
+                  ],
+                  title: 'Surgeries',
+                ),
+                OptionsWidget(
+                  options: [
+                    ["Yes", "yes"],
+                    ["No", "no"],
+                  ],
+                  title: 'Aware about Anganwadi services?',
+                ),
+                OptionsWidget(
+                  options: [
+                    ["Yes", "yes"],
+                    ["No", "no"],
+                  ],
+                  title: 'Using anything?',
+                ),
+                //TODO: Display next widget only if the previous widget is true
+                //TODO: Find out list of Anganwadi services, list em in a checkerbox dialog
+
+              ],
+            )
+          ], _onSubmit),
         ),
       ),
-        );
+    );
   }
 }
