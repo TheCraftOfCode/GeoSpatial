0e266fc6e455d5bac6f31ea4ab80992bad5c04fe
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2021-11-16 01:40:08 +0530

    Added FamilyDetails.dart and FamilyHomeScreen.dart

---
+++ lib/Screens/FamilyDetails.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FamilyDetails extends StatelessWidget {
+  const FamilyDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
