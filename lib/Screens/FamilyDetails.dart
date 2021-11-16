64e0572a4dc18baca2d99cf24aea21a188ae4a87
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2021-11-16 12:17:19 +0530

    Added widgets

--- lib/Screens/FamilyDetails.dart
+++ lib/Screens/FamilyDetails.dart
@@ -19,9 +19,8 @@ class _FamilyDetailsState extends State<FamilyDetails> {
   final PageController controller = PageController(initialPage: 0);
 
   final List<GlobalObjectKey<FormState>> formKeyList =
-  List.generate(3, (index) => GlobalObjectKey<FormState>(index));
+      List.generate(3, (index) => GlobalObjectKey<FormState>(index));
   final error = [false, false, false];
-
 
   @override
   Widget build(BuildContext context) {
@@ -29,132 +28,122 @@ class _FamilyDetailsState extends State<FamilyDetails> {
         resizeToAvoidBottomInset: false,
         backgroundColor: Color(0xffEAE7FA),
         appBar: AppBarBackButton('Add Family Member'),
-      body:SizedBox(
-    height: MediaQuery.of(context).size.height -
-    MediaQuery.of(context).viewInsets.bottom * 1.1,
-    child: SingleChildScrollView(
-    physics: ClampingScrollPhysics(),
-    child: FormPageView([
-      Column(
-    children: [
-      OptionsWidget(options: [
-        ['Yes', 'yes'],
-        ['No', 'no']
-      ], title: "Do you have drinking water available?"),
-      //TODO: Source of water if answer is yes
-      OptionsWidget(options: [
-        ['Yes', 'yes'],
-        ['No', 'no']
-      ], title: "Do you have toilet facilities?"),
-      OptionsWidget(options: [
-        ['Yes', 'yes'],
-        ['No', 'no']
-      ], title: "Is running water available?"),
-      OptionsWidget(options: [
-        ['Yes', 'yes'],
-        ['No', 'no']
-      ], title: "If not toilet, community toilet?"),
-      OptionsWidget(options: [
-        ['Clean', 'clean'],
-        ['Unclean', 'unclean']
-      ], title: "Environmental sanitation level: "),
-      DropDownFormField(
-        list: [
-          '1',
-          '2',
-          '3',
-          '4',
-          '5',
-          'More'
-        ],
-        hint: "Select the highest",
-        title: "No of two wheelers",
-        errorField: "Please choose a valid number",
-      ),
-      DropDownFormField(
-        list: [
-          '1',
-          '2',
-          '3',
-          '4',
-          '5',
-          'More'
-        ],
-        hint: "Select the highest",
-        title: "No of three wheelers",
-        errorField: "Please choose a valid number",
-      ),
-      //TODO: Add dialog checkboxlist for brands of two and three wheelers
-      DropDownFormField(
-        list: [
-          '1',
-          '2',
-          '3',
-          '4',
-          '5',
-          'More'
-        ],
-        hint: "Select the highest",
-        title: "No of four wheelers",
-        errorField: "Please choose a valid number",
-      ),
-      //TODO: Add brands of four wheelers
-      OptionsWidget(options: [['Yes','yes'],['No','no']], title: "Do you own cattle?"),
-      //TODO: Display if answer to previous question is yes
-      TextFormField(
-        decoration: InputDecoration(
-          label: Text('Income from cattle: ')
-        ),
-        autovalidateMode: AutovalidateMode.always,
-        keyboardType: TextInputType.number,
-      ),
-      OptionsWidget(
-        options: [
-          ['Yes','yes'],
-          ['No','no']
-        ],
-        title: "Do you own farmland?",
-      ),
-      //TODO: checkbox dialog for crops cultivated
-      OptionsWidget(
-        options: [
-          ['Yes','yes'],
-          ['No','no']
-        ],
-        title: "Do you preserve seeds?",
-      ),
-      //TODO: Display if answer to previous question is yes
-      //TODO: Add autosuggest text widget for seeds preserved
-      //TODO: Add autosuggest text widget for locally consumed food
-      //TODO: Add autosuggest text widget for trees owned
-      OptionsWidget(
-        options: [
-          ['Yes','yes'],
-          ['No','no']
-        ],
-        title: "Do you have a kitchen garden?",
-      ),
-      //TODO: Add autosuggest text widget for kitchen garden crops
-      TextFormField(
-        decoration: InputDecoration(
-          label: Text('First line'),
-        ),
-      ),
-      TextFormField(
-        decoration: InputDecoration(
-          label: Text('Second line'),
-        ),
-      ),
-      TextFormField(
-        decoration: InputDecoration(
-          label: Text('City'),
-        ),
-      ),
-    ],
-    )
-    ],_onSubmit)
-    )
-      )
-    );
+        body: SizedBox(
+            height: MediaQuery.of(context).size.height -
+                MediaQuery.of(context).viewInsets.bottom * 1.1,
+            child: SingleChildScrollView(
+                physics: ClampingScrollPhysics(),
+                child: FormPageView([
+                  Column(
+                    children: [
+                      OptionsWidget(options: [
+                        ['Yes', 'yes'],
+                        ['No', 'no']
+                      ], title: "Do you have drinking water available?"),
+                      //TODO: Source of water if answer is yes
+                      OptionsWidget(options: [
+                        ['Yes', 'yes'],
+                        ['No', 'no']
+                      ], title: "Do you have toilet facilities?"),
+                      OptionsWidget(options: [
+                        ['Yes', 'yes'],
+                        ['No', 'no']
+                      ], title: "Is running water available?"),
+                      OptionsWidget(options: [
+                        ['Yes', 'yes'],
+                        ['No', 'no']
+                      ], title: "If not toilet, community toilet?"),
+                      OptionsWidget(options: [
+                        ['Clean', 'clean'],
+                        ['Unclean', 'unclean']
+                      ], title: "Environmental sanitation level: "),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      DropDownFormField(
+                        list: ['1', '2', '3', '4', '5', 'More'],
+                        hint: "Select the highest",
+                        title: "No of two wheelers",
+                        errorField: "Please choose a valid number",
+                      ),
+                      DropDownFormField(
+                        list: ['1', '2', '3', '4', '5', 'More'],
+                        hint: "Select the highest",
+                        title: "No of three wheelers",
+                        errorField: "Please choose a valid number",
+                      ),
+                      //TODO: Add dialog checkboxlist for brands of two and three wheelers
+                      DropDownFormField(
+                        list: ['1', '2', '3', '4', '5', 'More'],
+                        hint: "Select the highest",
+                        title: "No of four wheelers",
+                        errorField: "Please choose a valid number",
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      //TODO: Add brands of four wheelers
+                      OptionsWidget(options: [
+                        ['Yes', 'yes'],
+                        ['No', 'no']
+                      ], title: "Do you own cattle?"),
+                      //TODO: Display if answer to previous question is yes
+                      TextFormField(
+                        decoration: InputDecoration(
+                            label: Text('Income from cattle: ')),
+                        autovalidateMode: AutovalidateMode.always,
+                        keyboardType: TextInputType.number,
+                      ),
+                      OptionsWidget(
+                        options: [
+                          ['Yes', 'yes'],
+                          ['No', 'no']
+                        ],
+                        title: "Do you own farmland?",
+                      ),
+                      //TODO: checkbox dialog for crops cultivated
+                      OptionsWidget(
+                        options: [
+                          ['Yes', 'yes'],
+                          ['No', 'no']
+                        ],
+                        title: "Do you preserve seeds?",
+                      ),
+                      //TODO: Display if answer to previous question is yes
+                      //TODO: Add autosuggest text widget for seeds preserved
+                      //TODO: Add autosuggest text widget for locally consumed food
+                      //TODO: Add autosuggest text widget for trees owned
+                      OptionsWidget(
+                        options: [
+                          ['Yes', 'yes'],
+                          ['No', 'no']
+                        ],
+                        title: "Do you have a kitchen garden?",
+                      ),
+                      //TODO: Add autosuggest text widget for kitchen garden crops
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                          label: Text('First line'),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          label: Text('Second line'),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          label: Text('City'),
+                        ),
+                      ),
+                    ],
+                  )
+                ], _onSubmit))));
   }
 }
