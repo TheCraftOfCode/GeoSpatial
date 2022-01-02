296abeb578e26b850b6da9b0cd27dd18b9a6e430
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2022-01-02 16:32:24 +0530

    Started adding biometrics

--- lib/Screens/Login.dart
+++ lib/Screens/Login.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
-
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:geo_spatial/Constants/Constants.dart';
@@ -15,6 +16,11 @@ class Login extends StatefulWidget {
   @override
   _MyAppState createState() => new _MyAppState();
 }
+enum _SupportState {
+  unknown,
+  supported,
+  unsupported,
+}
 
 class _MyAppState extends State<Login> {
   final TextEditingController _usernameController = TextEditingController();
@@ -24,9 +30,97 @@ class _MyAppState extends State<Login> {
   var _passwordError = null;
   bool _isSelected = false;
 
+
+  final LocalAuthentication auth = LocalAuthentication();
+  _SupportState _supportState = _SupportState.unknown;
+  bool? _canCheckBiometrics;
+  List<BiometricType>? _availableBiometrics;
+  String _authorized = 'Not Authorized';
+  bool _isAuthenticating = false;
+
+
   @override
   void initState() {
     super.initState();
+    auth.isDeviceSupported().then(
+          (bool isSupported) => setState(() => _supportState = isSupported
+          ? _SupportState.supported
+          : _SupportState.unsupported),
+    );
+  }
+
+  Future<void> _checkBiometrics() async {
+    late bool canCheckBiometrics;
+    try {
+      canCheckBiometrics = await auth.canCheckBiometrics;
+    } on PlatformException catch (e) {
+      canCheckBiometrics = false;
+      print(e);
+    }
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _canCheckBiometrics = canCheckBiometrics;
+    });
+  }
+
+  Future<void> _getAvailableBiometrics() async {
+    late List<BiometricType> availableBiometrics;
+    try {
+      availableBiometrics = await auth.getAvailableBiometrics();
+    } on PlatformException catch (e) {
+      availableBiometrics = <BiometricType>[];
+      print(e);
+    }
+    if (!mounted) {
+      return;
+    }
+
+    setState(() {
+      _availableBiometrics = availableBiometrics;
+    });
+  }
+
+  Future<void> _authenticateWithBiometrics() async {
+    bool authenticated = false;
+    try {
+      setState(() {
+        _isAuthenticating = true;
+        _authorized = 'Authenticating';
+      });
+      authenticated = await auth.authenticate(
+          localizedReason:
+          'Scan your fingerprint (or face or whatever) to authenticate',
+          useErrorDialogs: true,
+          stickyAuth: true,
+          biometricOnly: true);
+      setState(() {
+        _isAuthenticating = false;
+        _authorized = 'Authenticating';
+      });
+    } on PlatformException catch (e) {
+      print(e);
+      setState(() {
+        _isAuthenticating = false;
+        _authorized = 'Error - ${e.message}';
+      });
+      return;
+    }
+    if (!mounted) {
+      return;
+    }
+
+    final String message = authenticated ? 'Authorized' : 'Not Authorized';
+    setState(() {
+      _authorized = message;
+    });
+  }
+
+  Future<void> _cancelAuthentication() async {
+    await auth.stopAuthentication();
+    setState(() => _isAuthenticating = false);
   }
 
   void _radio() {
@@ -215,7 +309,10 @@ class _MyAppState extends State<Login> {
                         right: MediaQuery.of(context).size.width * 0.125,
                         bottom: 30),
                     child: IconButton(
-                        onPressed: () {},
+                        onPressed: () async {
+                          //bool canCheckBiometrics =
+                              //await localAuth.canCheckBiometrics;
+                        },
                         icon: Icon(
                           Icons.fingerprint_rounded,
                           size: 80,
