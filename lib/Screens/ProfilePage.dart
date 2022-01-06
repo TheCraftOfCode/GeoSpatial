f598b85ecd1b5af0efe028b931cb5ee58e951a5d
Nirmal K <nirmalkarthikeyan1503@gmail.com>
2022-01-06 12:16:37 +0530

    Profile page - WIP

--- lib/Screens/ProfilePage.dart
+++ lib/Screens/ProfilePage.dart
@@ -1,29 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:geo_spatial/Utils/Colors.dart' as colors;
 import 'package:geo_spatial/Utils/Globals.dart' as globals;
-
+import 'package:geo_spatial/Widgets/AppBarBackButtonWidget.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height*0.3,
-          ),
-          Container(
-            height: MediaQuery.of(context).size.height*0.1,
-          ),
-          Text("Name goes here"),
-          Text("Username in smaller, less bright text"),
-        ],
-      ),
+      body: Stack(children: [
+        SvgPicture.asset(
+          'assets/svg/profile_bg.svg',
+          alignment: Alignment.center,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppBarBackButton('Profile'),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.08,
+            ),
+            Center(
+              child: Column(
+                children: [
+                  Image(image: AssetImage("assets/avatar_woman.png")),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15.0),
+                    child: Text(
+                      globals.Name,
+                      style: GoogleFonts.poppins(fontSize: 20),
+                    ),
+                  ),
+                  Text(
+                    '@' + globals.userName,
+                    style: GoogleFonts.poppins(
+                        fontSize: 15, color: colors.darkSecondaryTextColor),
+                    textAlign: TextAlign.start,
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 40.0),
+              child: Row(
+                children: [
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      primary: colors.darkSecondaryTextColor.withOpacity(0.2),
+                    ),
+                    onPressed: () {
+                      print('Button Clicked');
+                    },
+                    child: Text(
+                      'Change\nPassword',
+                      style: GoogleFonts.poppins(
+                          color: colors.darkPrimaryTextColor),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Joined on: '),
+                  Text('Records collected: '),
+                ],
+              ),
+            )
+          ],
+        ),
+      ]),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:geo_spatial/Screens/Login.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:geo_spatial/Screens/Home.dart';
 import 'package:flutter/services.dart';
+import 'package:geo_spatial/Screens/ProfilePage.dart';
 import 'package:geo_spatial/Utils/DarkTheme.dart';
 import 'package:geo_spatial/Widgets/LoadValidPageWidget.dart';
 
@@ -14,7 +15,8 @@ void main() {
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
     runApp(MaterialApp(
-        home: LoadValidPageWidget(Login(), Home()),
+        home: ProfilePage(),
+        //LoadValidPageWidget(Login(), Home()),
         debugShowCheckedModeBanner: false,
         theme: darkTheme.DarkTheme
     ));
