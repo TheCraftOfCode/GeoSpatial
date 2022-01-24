eb214230be2fea7f56826eea2b59f1b27f96f8a8
sooryasrajan <soorya.s27@gmail.com>
2022-01-24 17:51:11 +0530

    Changed Deployment endpoint for testing

--- lib/Utils/Constants.dart
+++ lib/Utils/Constants.dart
@@ -1,5 +1,6 @@
 //Server ID Address/URL, change here and this constant, avoid hardcoding
-String NETWORK_ADDRESS = 'geo-spatial-amrita.herokuapp.com';
+//String NETWORK_ADDRESS = 'geo-spatial-amrita.herokuapp.com';
+String NETWORK_ADDRESS = 'geo-spatial-avv.herokuapp.com';
 //String NETWORK_ADDRESS = '192.168.29.126:2000';
 String JWT_STORAGE_KEY = 'jwt';
 String USER_ID_KEY = "userId";
